6595838adebaeb28124f8a748a0c808333811d50
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-10 21:08:05 -0600

    working on service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,6 +1,8 @@
 const APP_PREFIX = "FoodFest-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
 const FILES_TO_CACHE = [
   "/",
@@ -11,3 +13,12 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
