5c6de60730ac7c33f31ed40948db879a1543e8bf
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-11 17:39:19 -0600

    fixing deployment

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-let theTransactions = [];
+let transactions = [];
 let myChart;
 
 fetch("/api/transaction")
@@ -7,7 +7,7 @@ fetch("/api/transaction")
   })
   .then((data) => {
     // save db data on global variable
-    theTransactions = data;
+    transactions = data;
 
     populateTotal();
     populateTable();
@@ -16,7 +16,7 @@ fetch("/api/transaction")
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  let total = theTransactions.reduce((total, t) => {
+  let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
 
@@ -28,7 +28,7 @@ function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
-  theTransactions.forEach((transaction) => {
+  transactions.forEach((transaction) => {
     // create and populate a table row
     let tr = document.createElement("tr");
     tr.innerHTML = `
@@ -42,7 +42,7 @@ function populateTable() {
 
 function populateChart() {
   // copy array and reverse it
-  let reversed = theTransactions.slice().reverse();
+  let reversed = transactions.slice().reverse();
   let sum = 0;
 
   // create date labels for chart
@@ -106,7 +106,7 @@ function sendTransaction(isAdding) {
   }
 
   // add to beginning of current array of data
-  theTransactions.unshift(transaction);
+  transactions.unshift(transaction);
 
   // re-run logic to populate ui with new record
   populateChart();
