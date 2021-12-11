6328a81e9c383a901879429fa176899404c00b55
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-10 21:21:39 -0600

    finished service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -43,3 +43,23 @@ self.addEventListener("activate", (event) => {
       .then(() => self.clients.claim())
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
