c37c7fa43371cba605f0c953aa7a69ba2f0fa951
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-10 20:57:25 -0600

    configuring the service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,3 +1,13 @@
 const APP_PREFIX = "FoodFest-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/js/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
